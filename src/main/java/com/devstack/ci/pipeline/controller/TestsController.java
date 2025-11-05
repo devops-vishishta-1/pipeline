5ff68c957f14f3f6474ebd1d4ff5767ce6a67b6a
VishishtaDilsara <rgvdilsara2002@gmail.com>
2025-11-05 18:07:05 +0530

    Commit 1

--- src/main/java/com/devstack/ci/pipeline/controller/TestsController.java
+++ src/main/java/com/devstack/ci/pipeline/controller/TestsController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestsController {
     @GetMapping
     public String test(){
-        return "Success...";
+        return "Success Test 1!...";
     }
 }
